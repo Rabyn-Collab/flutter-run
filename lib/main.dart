ef5ef71bb848db4fef499ef621586ca28ae22136
Rabyn-Collab <babynshrestha64@gmail.com>
2022-12-21 16:14:25 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutterrun/models/movie_state.dart';
-import 'package:flutterrun/providers/example_providers.dart';
-import 'package:flutterrun/services/movie_service.dart';
 import 'package:flutterrun/view/home_page.dart';
 import 'package:get/get.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'api.dart';
+import 'firebase_options.dart';
 
 
-Future<String> delaySome() async{
- return Future.delayed(Duration(seconds: 5), (){
-   return 'hello world';
-  });
-}
 
-
-// MovieState newState = MovieState(err: '', isLoad: false, movies: []);
-//
-// void m(){
-//  newState =  newState.copyWith( err: 'hello', movieState: newState);
-// }
 
 void main() async{
   await Future.delayed(Duration(milliseconds: 500));
-  await Hive.initFlutter();
 
-  final box = await Hive.openBox<String>('movieBox');
-  // print(newState.err);
-  // m();
-  // print(newState.isLoad);
- //  print('hello world');
- // final m = await delaySome();
- // print(m);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
 
-   // final response = await MovieService.getMovieByCategory(apiPath: Api.popularMovie, page: 1);
-   //   response.fold((l) => print(l), (r) => print(r));
   runApp(ProviderScope(
       child: Home()
   )
@@ -65,54 +44,5 @@ class Home extends StatelessWidget {
 }
 
 
-
-
-
-
-
-
-
-
-class Counter extends StatelessWidget {
-  const Counter({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-          child:  Consumer(
-            builder: (context, ref, child) {
-             // final number = ref.watch(sampleProvider).number;
-            //  final number = ref.watch(changeProvider).number;
-              final number = ref.watch(stateProvider);
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('$number', style: TextStyle(fontSize: 40),),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                           // ref.read(sampleProvider).increment();
-                          //  ref.read(changeProvider).increment();
-                            ref.read(stateProvider.notifier).state++;
-                          }, child: Text('Incre')),
-                      TextButton(
-                          onPressed: () {
-                            //ref.read(sampleProvider).decrement();
-                           // ref.read(changeProvider).decrement();
-                            ref.read(stateProvider.notifier).state--;
-                          }, child: Text('Decre')),
-                    ],
-                  )
-                ],
-              );
-            }
-          ),
-        )
-    );
-  }
-}
 
 
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutterrun/view/search_page.dart';
-import 'package:flutterrun/view/widgets/popular_widget.dart';
-import 'package:flutterrun/view/widgets/tabs_widget.dart';
-import 'package:get/get.dart';
-import '../providers/movie_provider.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 
@@ -13,50 +7,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove();
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 79,
-          flexibleSpace: Container(
-            margin: EdgeInsets.only(bottom: 20),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('MovieShow', style: TextStyle(color: Colors.red, fontSize: 22, fontWeight: FontWeight.bold, letterSpacing: 1),),
-                    IconButton(onPressed: (){
-                      Get.to(() => SearchPage(), transition: Transition.leftToRight);
-                    }, icon: Icon(Icons.search_outlined))
-                  ],
-                ),
-              ),
-            ),
-          ),
-          bottom: TabBar(
-            physics: NeverScrollableScrollPhysics(),
-            indicator: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(20)
-            ),
-              tabs: [
-                Tab(text: 'Popular'),
-                Tab(text: 'Top_rated'),
-                Tab(text: 'UpComing'),
-              ]
-          ),
-        ),
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-              children: [
-            PopularWidget(popularProvider, 'popular'),
-            TabsWidget(topProvider, 'top'),
-            TabsWidget(upcomingProvider, 'upcoming'),
-          ])
-      ),
+    return Scaffold(
+        body: Container()
     );
   }
 }
