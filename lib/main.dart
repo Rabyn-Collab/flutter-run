445ad66d088252dcf2a26ac3baa50fec1a68cb90
Rabyn-Collab <babynshrestha64@gmail.com>
2023-01-09 15:57:42 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,18 @@
-import 'dart:async';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutterrun/view/status_page.dart';
-import 'package:get/get.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutterrun/view/home_page.dart';
+import 'package:flutterrun/view/status_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:get/get.dart';
 
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-
-
-}
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  "High_importance_channel",
-  "High_importance_channel",
-  importance: Importance.high,
-);
-
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
-
-const InitializationSettings initializationSettings =
-InitializationSettings(
-  android: AndroidInitializationSettings("@mipmap/ic_launcher"),
-);
-
-void main() async{
-  await Future.delayed(Duration(milliseconds: 500));
+void main () async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-
-  runApp(ProviderScope(
-      child: Home()
-  )
-  );
-
-
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-  flutterLocalNotificationsPlugin.initialize(initializationSettings);
-
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-
-    )
-  );
+  await Future.delayed(Duration(milliseconds: 500));
+  await Hive.initFlutter();
+  runApp(ProviderScope(child: Home(),));
 }
-
 
 
 class Home extends StatelessWidget {
@@ -64,39 +20,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: StatusPage(),
+    return ScreenUtilInit(
+      designSize: const Size(411, 866),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context , child) {
+        return GetMaterialApp(
+          theme: ThemeData(
+            fontFamily: ''
+          ),
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
+      child: StatusPage(),
     );
   }
 }
-
-
-
-class Counter extends StatelessWidget {
-
-  StreamController<int> numbers = StreamController();
- int numSome =0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: StreamBuilder<Object>(
-          stream: numbers.stream,
-          builder: (context, snapshot) {
-            final number = snapshot.data;
-            print(snapshot.data);
-            return Center(
-                child: Text('$number', style: TextStyle(fontSize: 20),)
-            );
-          }
-        ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
-           numbers.sink.add(numSome++);
-          },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
